ed6b823aa866c8c946ce6ff77dc53aa399c734fe
Staphey <117002522+Staphey@users.noreply.github.com>
2025-06-25 06:09:02 +0000

    feat(page): removed a few components to display

--- src/app/components/ui/User.tsx
+++ src/app/components/ui/User.tsx
@@ -11,11 +11,7 @@ export type UserType = {
 function User({ id, name, username, email, website}: UserType) {
     return (
         <div>
-        <div>{id}</div>
         <div>{name}</div>
-        <div>{username}</div>
-        <div className="hover:text-blue-500 underline">{email}</div>
-        <div>{website}</div>
         </div>
     )
 }
